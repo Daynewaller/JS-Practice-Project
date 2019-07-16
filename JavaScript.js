c065ac2560247e29d7495018c8e5947a9714bb1e
Daynewaller <52458062+Daynewaller@users.noreply.github.com>
2019-07-16 06:36:37 -0700

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -53,3 +53,26 @@ DOCTYPE html>
                     
                     
                     
+                    
+                    
+                    
+                    
+                    
+                    
+                    <ul>
+                                                         <li><a href"index.html">home</a></li>
+                                                         <li><a href"#">Portfolio</a>
+                                                                       <ul>
+                                                                      <li><a href"#">Digital art</a></li> 
+                                                                      <li><a href"#">Video production</a></li>
+                                                                      <li><a href"#">web devlopment</a></li>
+                                                                       </ul>
+                                                                       </li>
+                                                         <li><a href"#">About me</a></li>              
+                                                         <li><a href"#">Contact</a></li>
+                                                     </ul>
+                                                                       </naV>
+<body>
+        </html>
+
+                    
